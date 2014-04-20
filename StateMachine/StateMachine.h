aa92608cd6b173b56b9ed7ab0d152b54c78256e5
Ronaldo Faria Lima <ronaldo@saibre.com.br>
2014-04-20 20:47:36 -0300

    Initial Commit

---
+++ StateMachine/StateMachine.h
@@ -0,0 +1,13 @@
+//
+//  StateMachine.h
+//  StateMachine
+//
+//  Created by Ronaldo Faria Lima on 20/04/14.
+//  Copyright (c) 2014 Ronaldo Faria Lima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StateMachine : NSObject
+
+@end
---
+++ StateMachine/StateMachine.m
@@ -0,0 +1,13 @@
+//
+//  StateMachine.m
+//  StateMachine
+//
+//  Created by Ronaldo Faria Lima on 20/04/14.
+//  Copyright (c) 2014 Ronaldo Faria Lima. All rights reserved.
+//
+
+#import "StateMachine.h"
+
+@implementation StateMachine
+
+@end
